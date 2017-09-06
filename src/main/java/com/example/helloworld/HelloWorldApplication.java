9b17a6ee90d714da19a1af2f43bfed6c3079e3e4
Irvi Firqotul Aini <irvi.fa@gmail.com>
2017-09-06 16:09:26 +0700

    add swagger for openapi

--- src/main/java/com/example/helloworld/HelloWorldApplication.java
+++ src/main/java/com/example/helloworld/HelloWorldApplication.java
@@ -6,6 +6,23 @@ import io.dropwizard.Application;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
 
+import io.federecio.dropwizard.swagger.SwaggerBundle;
+import io.federecio.dropwizard.swagger.SwaggerBundleConfiguration;
+
+import com.wordnik.swagger.annotations.Api;
+import com.wordnik.swagger.annotations.ApiOperation;
+import com.wordnik.swagger.annotations.ApiParam;
+
+import javax.ws.rs.Consumes;
+import javax.ws.rs.FormParam;
+import javax.ws.rs.GET;
+import javax.ws.rs.POST;
+import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+
 public class HelloWorldApplication extends Application<HelloWorldConfiguration> {
   public static void main(String[] args) throws Exception {
     new HelloWorldApplication().run(args);
@@ -19,8 +36,19 @@ public class HelloWorldApplication extends Application<HelloWorldConfiguration> 
   @Override
   public void initialize(Bootstrap<HelloWorldConfiguration> bootstrap) {
     // nothing to do yet
+    bootstrap.addBundle(new SwaggerBundle<HelloWorldConfiguration>() {
+      @Override
+      protected SwaggerBundleConfiguration getSwaggerBundleConfiguration(HelloWorldConfiguration sampleConfiguration) {
+        // this would be the preferred way to set up swagger, you can also construct the object here programtically if you want
+        return sampleConfiguration.swaggerBundleConfiguration;
+      }
+    });
+    
   }
 
+  @GET
+  @ApiOperation("Sample endpoint with query param")
+  @Path("/hello-world")
   @Override
   public void run(HelloWorldConfiguration configuration,
                   Environment environment) {
